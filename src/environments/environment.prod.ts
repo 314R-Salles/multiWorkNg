891ed9e687295f55d5de00dafc1d2c5e030a3d49
314R-Salles <sallespierre@live.fr>
2021-10-10 17:21:55 +0200

    add link and ?. in html

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,5 @@ export const environment = {
   TWITCH_AUTH_URL: 'https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=048o30kiq54suyv43jio7boaknv8e2&redirect_uri=https://psalles.ovh&state=c3ab8aa609ea11e793ae92361f002671',
   JAVA_API: 'https://psalles.ovh:8080',
   TWITCH_PARENT: 'psalles.ovh',
-  IPGEOLOCATION_API_KEY: 'XXXXXXXXXXXXXXXXXXXXX',
+  IPGEOLOCATION_API_KEY: 'IPGEOLOCATION_API_KEY',
 };
